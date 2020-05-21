cc0dd3190daf709ef825264392aa84ef006cf456
HectorCan <habacab@gmail.com>
2020-05-20 21:41:41 -0500

    Project Initialization and CoreUI Routes

---
+++ resources/views/dashboard/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('dashboard.app')
+
+@section('content')
+    <div class="container-fluid">
+
+    </div>
+@endsection
+
+@section('javascript')
+
+    <script src="{{ asset('js/Chart.min.js') }}"></script>
+    <script src="{{ asset('js/coreui-chartjs.bundle.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
+@endsection
